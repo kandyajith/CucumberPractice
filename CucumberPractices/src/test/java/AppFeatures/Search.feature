2f585ab89faee9ee1bdf63bca49f0153dc08bed8
kandyajith <kandyajith@hotmail.com>
2021-09-01 13:32:32 +0100

    First commit

---
+++ CucumberPractices/src/test/java/AppFeatures/Search.feature
@@ -0,0 +1,5 @@
+Feature: Amazon Search
+Scenario: Search a Product
+Given I have a search field on Amazon page
+When I search for a product with name "Apple MacBook Pro" and price 1000
+Then Product with "Apple  MacBook Pro" should be displayed
---
+++ CucumberPractices/src/test/java/stepDefinitions/OrderSteps.java
@@ -0,0 +1,57 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class OrderSteps 
+{
+	
+	@Given("a registed user exists")
+	public void a_registed_user_exists() {
+	}
+
+	@Given("user is on Amason login page")
+	public void user_is_on_amason_login_page() {
+	}
+
+	@When("user enters username")
+	public void user_enters_username() {
+	}
+
+	@When("user enters password")
+	public void user_enters_password() {
+	}
+
+	@When("user click on login button")
+	public void user_click_on_login_button() {
+	}
+
+	@Then("user navigates to orger page")
+	public void user_navigates_to_orger_page() {
+	}
+
+	@When("user clicks on Order link")
+	public void user_clicks_on_order_link() {
+	}
+
+	@Then("user checks the previous order details")
+	public void user_checks_the_previous_order_details() {
+	}
+
+	@When("user clicks on open Order link")
+	public void user_clicks_on_open_order_link() {
+	}
+
+	@Then("user checks the open order details")
+	public void user_checks_the_open_order_details() {
+	}
+
+	@When("user clicks on cancelled Order link")
+	public void user_clicks_on_cancelled_order_link() {
+	}
+
+	@Then("user checks the cancelled order details")
+	public void user_checks_the_cancelled_order_details() {
+	}
+
+
+}
